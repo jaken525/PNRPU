2348d893a984f06ca55ac1b90c28988cc0aae442
Povazhnyy Vitaliy <57368196+jaken525@users.noreply.github.com>
2023-05-11 14:25:34 +0500

    Add files via upload

---
+++ ооп 12/ооп 12_1/ооп 12_1/ооп 12_1.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+typedef set<double> TSet;
+set<double>::iterator it;
+
+TSet make_set(int n)
+{
+	TSet m;
+	double a;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Number: ";
+		cin >> a;
+		m.insert(a);
+	}
+
+	return m;
+}
+
+void print_set(TSet m)
+{
+	for (it = m.begin(); it != m.end(); it++)
+		cout << *it << "  ";
+}
+
+TSet erase_set(TSet m, double el)
+{
+	for (it = m.begin(); it != m.end();)
+	{
+		double n = *it;
+		if (el == n)
+		{
+			cout << *it << endl;
+			m.erase(it++);
+		}
+		else
+			++it;
+	}
+
+	return m;
+}
+
+double Min_set(TSet m)
+{
+	double min, temp;
+	it = m.begin();
+	min = *it;
+
+	for (it = m.begin(); it != m.end(); ++it)
+	{
+		temp = *it;
+		if (min > temp)
+			min = temp;
+	}
+
+	return min;
+}
+
+double Max_set(TSet m)
+{
+	double max, temp;
+	it = m.begin();
+	max = *it;
+
+	for (it = m.begin(); it != m.end(); ++it)
+	{
+		temp = *it;
+		if (max < temp)
+			max = temp;
+	}
+
+	return max;
+}
+
+TSet zadanie(TSet m, double min, double max)
+{
+	for (it = m.begin(); it != m.end(); ++it)
+	{
+		double h = *it + min - max;
+		cout << h << "  ";
+	}
+
+	return m;
+}
+
+void main()
+{
+	int n;
+	cout << "Number: ";
+	cin >> n;
+
+	TSet m = make_set(n);
+	print_set(m);
+
+	double d;
+	cout << "Whats to remove: ";
+	cin >> d;
+
+	TSet j = erase_set(m, d);
+	print_set(j);
+	double h = Min_set(j);
+	double u;
+
+	cout << "Add: " << endl;
+	cin >> u;
+
+	j.insert(u);
+	print_set(j);
+
+	cout << endl;
+
+	double g = Max_set(j);
+
+	cout << "Add max and min: " << endl;
+	zadanie(j, h, g);
+}
---
+++ ооп 12/ооп 12_2/ооп 12_2/ооп 12_2.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <map>
+#include "Money.h"
+
+using namespace std;
+
+typedef map<int, Money> TMap;
+typedef TMap::iterator it;
+
+TMap make_map(int n)
+{
+	TMap m;
+	Money a;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		m.insert(make_pair(i, a));
+	}
+
+	return m;
+}
+
+void print_map(TMap m)
+{
+	for (int i = 0; i < m.size(); i++)
+		cout << i << " - " << m[i] << " " << endl;
+}
+
+Money middle(TMap m)
+{
+	Money s = m[0];
+
+	for (int i = 1; i < m.size(); i++)
+		s = s + m[i];
+
+	return s / m.size();
+}
+
+int Max(TMap v)
+{
+	it i = v.begin(); int nom = 0, k = 0;
+	Money m = (*i).second;
+	while (i != v.end())
+	{
+		if (m < (*i).second)
+		{
+			m = (*i).second;
+			nom = k;
+		}
+		i++;
+		k++;
+	}
+
+	return nom;
+}
+
+int Min(TMap v)
+{
+	it i = v.begin();
+	int nom = 0, k = 0;
+	Money m = (*i).second;
+
+	while (i != v.end())
+	{
+		if (m > (*i).second)
+		{
+			m = (*i).second;
+			nom = k;
+		}
+		i++;
+		k++;
+	}
+
+	return nom;
+}
+
+void delenie(TMap& v)
+{
+	Money m = v[Min(v)];
+	for (int i = 0; i < v.size(); i++)
+		v[i] = v[i] / m;
+}
+
+void main()
+{
+	int n;
+	cout << "Number: ";
+	cin >> n;
+
+	map<int, Money> m = make_map(n);
+	print_map(m);
+
+	Money el = middle(m);
+	cout << "Middle: " << el << endl;
+
+	m.insert(make_pair(n, el));
+	print_map(m);
+
+	int max = Max(m);
+	cout << "max=" << m[max] << " nom=" << max << endl;
+
+	m.erase(max);
+	print_map(m);
+
+	int min = Min(m);
+	cout << "min=" << m[min] << " nom=" << min << endl;
+
+	delenie(m);
+	print_map(m);
+}
