916ea12cb729d293d6fe234b7830a5f92eb99eb6
Povazhnyy Vitaliy <vitaliy.povazhnyy.93@mail.ru>
2024-05-17 18:48:42 +0500

    добавлена курсовая

--- второй курс лабы/ООП/VitalikTeamLibrary/VitalikTeamLibrary/obj/Debug/net6.0/VitalikTeamLibrary.AssemblyInfo.cs
+++ второй курс лабы/ООП/VitalikTeamLibrary/VitalikTeamLibrary/obj/Debug/net6.0/VitalikTeamLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitalikTeamLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532f92af54471f78675acd858069eff6fe63d555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823b74e8af993665f723cb870b37d6b9ce9355b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitalikTeamLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitalikTeamLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
